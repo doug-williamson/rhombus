d63a602219c090d87192337eabe35343d34e0760
Doug Williamson <douglasmwilliamson@gmail.com>
2020-02-04 16:52:39 -0500

    Sidenav toggle fix
    Fixed sidenav collapsing on routing. Commented out

--- projects/rhombus-shell/src/lib/core/components/shell-nav/shell-nav.component.ts
+++ projects/rhombus-shell/src/lib/core/components/shell-nav/shell-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { RhombusShellNavItem } from '../../models/shell-nav-item';
+import { RhombusShellNavService } from '../../services/nav.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'rhombus-shell-nav',
@@ -11,9 +13,13 @@ export class RhombusShellNavComponent implements OnInit {
   @Input()
   navItems: RhombusShellNavItem[];
 
-  constructor() { }
+  constructor(private router: Router, private sidenavService: RhombusShellNavService) { }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      // close sidenav on routing
+      this.sidenavService.close();
+    });
   }
 
 }
